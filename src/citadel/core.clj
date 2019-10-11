6c886308b3ee1d2c09308796a82a253d9d6e5085
Michael Bruce <me@michaelbruce.co>
2019-10-11 09:10:04 +0100

    lots of stuff commented out.. compiles executable

--- src/citadel/core.clj
+++ src/citadel/core.clj
@@ -1,60 +1,62 @@
 (ns citadel.core
-  (:require [clojure.java.shell :refer [sh]]
-            [clojure.java.io :as io])
-  (:refer-clojure :exclude [ensure]))
+  ;; (:require [clojure.java.shell :refer [sh]]
+  ;;           [clojure.java.io :as io])
+  ;; (:refer-clojure :exclude [ensure])
+  (:gen-class))
 
 ;; Tasks for Citadel
 ;; 1. Install selection of packages
 ;; 2. Warn/Remove undeclared packages
 ;; 3. Link configuration files from this repo => their destinations
 
-(def packages
-  [{:name "clojure"}
-   {:name "ripgrep"}
-   {:name "telegram-desktop"}
-   {:name "openjdk8-src" :comment "for Clojure source jumping"}
-   {:name "openjdk10-src" :comment "for Clojure source jumping"}
-   {:name "ttf-dejavu"}
-   {:name "ttf-liberation"}
-   {:name "noto-fonts"}
-   {:name "xautolock"}
-   {:name "bolt"}
-   {:name "inkscape"}])
-
-(defn refresh-database
-  "Loads fresh information about available Arch packages."
-  []
-  (sh "pacman" "-Sy"))
-
-(defn exists?
-  [package-name]
-  (zero? (:exit (sh "pacman" "-Qk" package-name))))
-
-(defn install
-  [package-name]
-  (let [process (sh "sudo" "pacman" "-S" "--noconfirm" package-name)]
-    (println process)
-    (zero? (:exit process))))
-
-(defn ensure
-  [package-name]
-  (when-not (exists? package-name)
-    (install package-name)))
-
-(defn read-deps [system-map]
-  (map first (:deps system-map)))
+;; (def packages
+;;   [{:name "clojure"}
+;;    {:name "ripgrep"}
+;;    {:name "telegram-desktop"}
+;;    {:name "openjdk8-src" :comment "for Clojure source jumping"}
+;;    {:name "openjdk10-src" :comment "for Clojure source jumping"}
+;;    {:name "ttf-dejavu"}
+;;    {:name "ttf-liberation"}
+;;    {:name "noto-fonts"}
+;;    {:name "xautolock"}
+;;    {:name "bolt"}
+;;    {:name "inkscape"}])
+;; 
+;; (defn refresh-database
+;;   "Loads fresh information about available Arch packages."
+;;   []
+;;   (sh "pacman" "-Sy"))
+;; 
+;; (defn exists?
+;;   [package-name]
+;;   (zero? (:exit (sh "pacman" "-Qk" package-name))))
+;; 
+;; (defn install
+;;   [package-name]
+;;   (let [process (sh "sudo" "pacman" "-S" "--noconfirm" package-name)]
+;;     (println process)
+;;     (zero? (:exit process))))
+;; 
+;; (defn ensure
+;;   [package-name]
+;;   (when-not (exists? package-name)
+;;     (install package-name)))
+;; 
+;; (defn read-deps [system-map]
+;;   (map first (:deps system-map)))
 
 ;; XXX this is now pretty big, since -main isn't tested let's thin this out.
 (defn -main
   "Entrypoint for "
   [& args]
   (println "Welcome. Test Print.")
-  (if-not (empty? args)
-    (let [map-path   (first args)
-          system-map (read-string (slurp (io/file map-path)))
-          deps       (map str (read-deps system-map))]
-      (println "--> Reading from" map-path)
-      (println deps)
-      (doseq [dep deps]
-        (ensure dep))))
-  (shutdown-agents)) ;; some processes hang around when using sudo
+  ; (if-not (empty? args)
+  ;   (let [map-path   (first args)
+  ;         system-map (read-string (slurp (io/file map-path)))
+  ;         deps       (map str (read-deps system-map))]
+  ;     (println "--> Reading from" map-path)
+  ;     (println deps)
+  ;     (doseq [dep deps]
+  ;       (ensure dep))))
+  ; (shutdown-agents)
+  ) ;; some processes hang around when using sudo
--- test/citadel/core_test.clj
+++ test/citadel/core_test.clj
@@ -1,14 +1,15 @@
 (ns citadel.core-test
-  (:require [clojure.test :refer [testing deftest is]]
-            [citadel.core :as citadel]
-            [clojure.java.io :as io]))
+  ;; (:require [clojure.test :refer [testing deftest is]]
+  ;;           [citadel.core :as citadel]
+  ;;           [clojure.java.io :as io])
+  )
 
-(deftest pacman-interop
-  (testing "detecting if a package is installed"
-    (is (citadel/exists? "bash"))
-    (is (not (citadel/exists? "not-a-program")))))
-
-(deftest ingests-system-edn
-  (testing "listing all dependencies"
-    (is (= (citadel/read-deps (read-string (slurp (io/resource "test-system.edn"))))
-           (list 'ripgrep 'telegram-desktop 'openjdk8-src)))))
+;; (deftest pacman-interop
+;;   (testing "detecting if a package is installed"
+;;     (is (citadel/exists? "bash"))
+;;     (is (not (citadel/exists? "not-a-program")))))
+;; 
+;; (deftest ingests-system-edn
+;;   (testing "listing all dependencies"
+;;     (is (= (citadel/read-deps (read-string (slurp (io/resource "test-system.edn"))))
+;;            (list 'ripgrep 'telegram-desktop 'openjdk8-src)))))
